08f2846e6e180eaff49183c626a9dcc9a2bf9d7a
Vitor Domingos Oliveira de Jesus <47833820857@info.local>
2026-03-17 20:18:32 -0300

    :test_tube: test: Teste da funcao funcional

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import processamento as process
 
 lista_alunos = [("", [90.00, 71.00, 50.00, 45.00]), ("Vinícius", [100.00, 100.00, 99.00, 90.00, 25.00]), ("Vitor", [25.00, 25.00, 70.00, 10.00])]
+
+process.varredura(lista_alunos)
