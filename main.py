9604a5cec2e659919f32441e17a893219a67302d
Vitor Domingos Oliveira de Jesus <47833820857@info.local>
2026-03-17 20:21:56 -0300

    :test_tube: test: Teste da funcao media funcional

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+import processamento as process
+
 lista_alunos = [("", [90.00, 71.00, 50.00, 45.00]), ("Vinícius", [100.00, 100.00, 99.00, 90.00, 25.00]), ("Vitor", [25.00, 25.00, 70.00, 10.00])]
+
+resultado = process.media(lista_alunos)
+print(resultado)
